55749de4afd7c62b425875dcea277050e1f3772b
Nasser-Mohammed <81037208+Nasser-Mohammed@users.noreply.github.com>
2025-04-27 23:57:03 -0500

    Add files via upload

--- _pages/programs-simulations/Turing-Patterns/main.js
+++ _pages/programs-simulations/Turing-Patterns/main.js
@@ -1,16 +1,16 @@
 
 
 
-let n = 400;
+let n = 800;
 let Dx = 1./n;
 let Dy = 1./n;
-let Dt = 0.02;
+let Dt = 0.0035;
 let alpha = 1.;
 let beta = -1.;
-let omega = 2.;
+let omega = 2.5;
 let lambda = -1.5;
-let h = 1.;
-let k = 1.;
+let h = .5;
+let k = .5;
 let sigma_u = 0.001;
 let sigma_v = 0.0006;
 let u = Array.from({ length: n }, () => Array(n).fill(0));
@@ -21,13 +21,15 @@ let ctx;
 let intervalId;
 let elapsedTime = 0;
 let isRunning = false;
+let reset = false;
+let paused = false;
 
 
 function initalizeSimulation(){
   for (let i = 0; i < n; i++){
     for (let j = 0; j < n; j++){
-      u[i][j] = 1.0 + Math.random()*(.03 + 0.03) - 0.03;
-      v[i][j] = 1.0 + Math.random()*(.03 + 0.03) - 0.03;
+      u[i][j] = 1.0 + Math.random()*(.5 + 0.5) - 0.5;
+      v[i][j] = 1.0 + Math.random()*(.5 + 0.5) - 0.5;
     }
   }
 
@@ -37,49 +39,46 @@ function drawGrid(u, v, ctx){
   const rows = u.length;
   const cols = u[0].length;
 
-  for (let i = 0; i < rows-6; i++){
-    for (let j = 0; j < cols-6; j++){
+  for (let i = 0; i < rows - 2; i = i+2){
+    for (let j = 0; j < cols - 2; j = j+2){
       const uVal = u[i][j];
       const vVal = v[i][j];
-
-      //const avg = (uVal + vVal)/2;
-      red = 0;
+      red = Math.min(Math.max(0, uVal), 255);;
       green = 0;
-      blue = 0;
-      if (uVal > vVal){
-        red = 255;
-        green = 128;
-        blue = 80;
-      }
-      else{
-        red = 0;
-        green = 128;
-        blue = 128;
-      }
-      //const red = max; //uVal;
-      //const green = 0;
-      //const blue = 0// vVal;
+      blue = Math.min(Math.max(0, vVal), 255);
+
       const blended = `rgb(${red}, ${green}, ${blue})`; 
       ctx.fillStyle = blended;
-      ctx.fillRect(j*3, i*3, 3, 3);
+      ctx.fillRect(j, i, 2, 2);
     }
   }
 }
 
 function updateTimer() {
-  elapsedTime++;  // Increment elapsed time
+  if(reset) {
+    elapsedTime = 0;  // Increment elapsed time
+  }
+  else{
+    elapsedTime++;  // Increment elapsed time
+  }
   document.getElementById('timer').innerText = `Time: ${elapsedTime}s`;  // Update timer on the page
+
 }
 
 function updateCanvas(u, v, ctx) {
-  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);  // Clear the canvas
+  if (reset){
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);  // Clear the canvas
+  }
   
   // Redraw the arrays (u and v) on the canvas
+  if(isRunning){
   drawGrid(u, v, ctx);
+  }
 }
 
 function update(){
   epsilon = 0.00001;
+  for(let step = 0; step < 20; step++){
   for (let i = 1; i < n-1; i++){
     for (let j = 1; j < n-1; j++){
       nextu[i][j] = u[i][j] + Dt*(sigma_u*((u[i+1][j] - 2*u[i][j] + u[i-1][j])/(Dx**2+epsilon) + (u[i][j+1] - 2*u[i][j] + u[i][j-1])/(Dy**2+epsilon)) + alpha*(u[i][j] - h) + beta*(v[i][j] - k));
@@ -87,12 +86,13 @@ function update(){
       
     }
   }
-  for (let i = 0; i < n-1; i++) {
-    for (let j = 0; j < n-1; j++) {
+  for (let i = 1; i < n-1; i++) {
+    for (let j = 1; j < n-1; j++) {
       u[i][j] = nextu[i][j];
       v[i][j] = nextv[i][j];
     }
   }
+}
   updateCanvas(u, v, ctx);
   updateTimer();
 }
@@ -110,15 +110,37 @@ document.addEventListener("DOMContentLoaded", () => {
         clearInterval(intervalId);
         this.textContent = "Resume Simulation";
         console.log('Paused');
+        paused = true;
       }
       else{
-        initalizeSimulation();
-        intervalId = setInterval(update, 1000);
+        if (!paused || reset){
+          initalizeSimulation();
+          updateCanvas(u,v,ctx);
+          started = true;
+        }
+        intervalId = setInterval(update, 60);
         console.log('Started');
         this.textContent = "Pause Simulation";
+        puased = false;
       }
       isRunning = !isRunning;
   });
-  initalizeSimulation();
+
+  document.getElementById('resetButton').addEventListener('click', function() {
+    if (isRunning){
+      clearInterval(intervalId);
+      console.log('Reset');
+      reset = true;
+      updateTimer();
+      isRunning = false;
+      started = false;
+      initalizeSimulation();
+      updateCanvas(u,v,ctx);
+      reset = false;
+      document.getElementById('startButton').textContent = "Start Simulation";
+      
+    }
+});
+  
 
 });
